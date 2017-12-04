d747482b25024c330061609086f53595cdb63f72
Marek Kubesa <marek.kubesa@gmail.com>
2017-12-04 11:11:07 +0100

    Back in the tower

--- server.js
+++ server.js
@@ -1,42 +1,19 @@
 var http = require('http');
 var url = require('url');
-var querystring = require('querystring');
-var markdown = require('markdown').markdown;
-var express = require('express');
-
 
 var server = http.createServer(function(req, res) {
-    var params = querystring.parse(url.parse(req.url).query);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-
-    if ('firstname' in params && 'lastname' in params) {
-        res.write('Your name is ' + params['firstname'] + ' ' + params['lastname']);
-        console.log(params['firstname']+ ' ' + params['lastname']);
-
-    }
-    else {
-        res.write('You do have a first name and a last name, don\'t you?');
-            console.log("Zadej "+ 'http://localhost:8080?firstname=John&lastname=Doe');
-    }
-
-
-    res.end();
+var page = url.parse(req.url).pathname;
+console.log(page);
+res.writeHead(200, {"Content-Type": "text/plain"});
+if (page == '/') {
+res.write('You\'re at the reception desk. How can I help you?');
+}
+else if (page == '/basement') {
+res.write('You\'re in the wine cellar. These bottles are mine!');
+}
+else if (page == '/floor/1/bedroom') {
+res.write('Hey, this is a private area!');
+}
+res.end();
 });
-
-server.on('close', function() { // We listened to the close event
-    console.log('Goodbye!');
-})
-
-console.log(markdown.toHTML('A paragraph in **markdown**!'));
-
-
-var app = express();
-
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('You\'re in reception');
-});
-
-server.listen(8080); // Starts the server
-
-//server.close(); // Stops the server. Triggers the close event
+server.listen(8080);
