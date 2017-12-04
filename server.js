13b59149e4aa019f29cefa852b9b7a2d6ed671f9
Marek Kubesa <marek.kubesa@gmail.com>
2017-12-04 11:02:15 +0100

    link with argument name FN+LN

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
+var markdown = require('markdown').markdown;
+var express = require('express');
+
 
 var server = http.createServer(function(req, res) {
     var params = querystring.parse(url.parse(req.url).query);
@@ -8,12 +11,32 @@ var server = http.createServer(function(req, res) {
 
     if ('firstname' in params && 'lastname' in params) {
         res.write('Your name is ' + params['firstname'] + ' ' + params['lastname']);
+        console.log(params['firstname']+ ' ' + params['lastname']);
+
     }
     else {
         res.write('You do have a first name and a last name, don\'t you?');
+            console.log("Zadej "+ 'http://localhost:8080?firstname=John&lastname=Doe');
     }
-    console.log(params['firstname']+ ' ' + params['lastname']);
+
 
     res.end();
 });
-server.listen(8080);
+
+server.on('close', function() { // We listened to the close event
+    console.log('Goodbye!');
+})
+
+console.log(markdown.toHTML('A paragraph in **markdown**!'));
+
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\'re in reception');
+});
+
+server.listen(8080); // Starts the server
+
+//server.close(); // Stops the server. Triggers the close event
