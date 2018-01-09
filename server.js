74977da4d43bda065d9ecd6da1edc86cebbfc4c6
bar2cha <bar2cha@email.cz>
2018-01-09 10:32:35 +0100

    fix ' (apostrophe)

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-var http = require('http');
-var url = require('url');
+var express = require('express');
 
-var server = http.createServer(function(req, res) {
-var page = url.parse(req.url).pathname;
-console.log(page);
-res.writeHead(200, {"Content-Type": "text/plain"});
-if (page == '/') {
-res.write('You\'re at the reception desk. How can I help you?');
-}
-else if (page == '/basement') {
-res.write('You\'re in the wine cellar. These bottles are mine!');
-}
-else if (page == '/floor/1/bedroom') {
-res.write('Hey, this is a private area!');
-}
-res.end();
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\'re in reception. How can I help you?');
 });
-server.listen(8080);
+
+app.get('/basement', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\'re in the wine cellar. Those bottles are mine!');
+});
+
+app.get('/floor/1/bedroom', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hey, this is a private area!');
+});
+
+app.listen(8080);
